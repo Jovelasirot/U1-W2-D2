0773af754bc7614ed3c4c854ee1e14125dfc13bb
Jovelasirot <jovelasirot@gmail.com>
2023-11-28 16:21:19 +0100

    ordering value in a descending order

--- D2.js
+++ D2.js
@@ -124,6 +124,38 @@ if (x > y && x > z) {
     }
   }
 }
+
+
+// separando gli if funziona
+
+let x = 10;
+let y = 25;
+let z = 15;
+
+if (x > y && x > z) {
+  if (y > z) {
+    console.log(x, y, z);
+  }
+  if (z > y) {
+    console.log(x, y, z);
+  }
+}
+if (y > x && y > z) {
+  if (x > z) {
+    console.log(y, x, z);
+  }
+  if (z > x) {
+    console.log(y, z, x);
+  }
+}
+if (z > y && z > x) {
+  if (x > y) {
+    console.log(z, x, y);
+  }
+  if (y > x) {
+    console.log(z, y, x);
+  }
+}
 */
 
 /* ESERCIZIO 8
